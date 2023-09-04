f02c284717ba90a2d096d80e6d0d93716d9bc345
mennaemam12 <mennaemam205@gmail.com>
2023-09-04 18:19:31 +0300

    .

--- public/js/readingMask.js
+++ public/js/readingMask.js
@@ -2,7 +2,32 @@ const readingMask = document.querySelector('.reading-mask');
 const parent=document.getElementsByClassName("row g-4");
 const categories=parent[0].childNodes;
 const texts=document.getElementsByClassName("text-center mb-5 wow fadeInUp");
-const jobs=document.getElementById("tab-1").children;
+const tab1=document.getElementById("tab-1");
+const jobs = tab1.querySelectorAll('.job-item.p-4.mb-4');
+const tab1JobItems = document.querySelectorAll('#tab-1 .job-item');
+const jobMask=document.getElementById("job-mask");
+
+var enabled=false;
+
+while(!enabled){
+    readingMask.style.display='none';
+    jobMask.style.display='none';
+}
+
+function buttonPressed(id){
+    if(!enabled){
+        document.getElementById(id).style.borderColor="green";
+        enabled=true;
+        readingMask.style.display='block';
+        jobMask.style.display='block';
+    }
+    else{
+        enabled=false;
+        readingMask.style.display='none';
+        jobMask.style.display='none';
+    }
+}
+
 
 
 categories.forEach(category=>{
@@ -17,35 +42,54 @@ categories.forEach(category=>{
     });
 })
 
-for(var i=0;i<4;i++){
-    jobs[i].addEventListener('mouseenter', ()=>{
-        console.log(jobs);
-        readingMask.innerHTML=jobs[i].innerHTML;
+console.log(jobs);
+jobs.forEach(job=>{
+    job.addEventListener('mouseenter', ()=>{
+        readingMask.innerHTML=job.innerHTML;
         //readingMask.querySelector('h5').style.fontSize='35px';
        
         readingMask.style.display = 'block';
     });
-    jobs[i].addEventListener('mouseleave', () => {
+    job.addEventListener('mouseleave', () => {
         readingMask.innerHTML="";
         readingMask.style.display = 'none';
     });
+});
+
+
+function handleJobItemHover(jobItem, readingmask) {
+    jobItem.addEventListener('mouseenter', () => {
+        const jobItemClone = jobItem.cloneNode(true);
+        const jobItemTitle = jobItemClone.querySelector('h5');
+        jobItemTitle.style.fontSize = '30px';
+        const text=jobItemClone.querySelectorAll('span');
+        text.forEach(tex=>{
+            tex.style.fontSize = '18px';
+        });
+        const smallElement=jobItemClone.querySelector('small');
+        smallElement.style.marginTop = '50px';
+        smallElement.style.fontSize = '14px';
+
+
+        readingmask.innerHTML = '';
+        readingmask.appendChild(jobItemClone);
+        readingmask.style.display = 'block';
+    });
+
+    jobItem.addEventListener('mouseleave', () => {
+        readingmask.innerHTML = '';
+        readingmask.style.display = 'none';
+    });
 }
 
-// texts.forEach(text=>{
-//     text.addEventListener('mouseenter', ()=>{
-//         readingMask.innerHTML=text.innerHTML;
-//         readingMask.style.fontSize='35px'
-//         readingMask.style.display = 'block';
-//     });
-//     text.addEventListener('mouseleave', () => {
-//         readingMask.innerHTML="";
-//         readingMask.style.display = 'none';
-//     });
-// })
+tab1JobItems.forEach(jobItem => {
+    handleJobItemHover(jobItem, jobMask);
+});
 
 // Function to update the reading mask position based on mouse movement
 function updateReadingMaskPosition(event) {
     readingMask.style.transform = `translate(${event.clientX}px, ${event.clientY}px)`;
+    jobMask.style.transform = `translate(${event.clientX}px, ${event.clientY}px)`;
 }
 
 
