e620abf231ae150bdebdfd8733522f78e9d96ae5
Mario Magdy <mariomagdyfa@gmail.com>
2023-09-07 01:21:01 +0300

    added

--- public/js/applied-for.js
+++ public/js/applied-for.js
@@ -1,6 +1,19 @@
 const expandIcon = document.querySelector('.comment-icon');
 const expandedContent = document.querySelector('.expanded-content');
 
+let ongo = sessionStorage.getItem('ongo');
+
+if(ongo == 'true'){
+   document.getElementById("changable").innerHTML="My Profile"
+   document.getElementById("changable").setAttribute("href", "myprofile");
+   document.getElementById("upload").style.display="flex";
+    document.getElementById("dropdown").style.display="flex";
+}
+else
+{
+    document.getElementById("upload").style.display="none";
+    document.getElementById("dropdown").style.display="none";
+}
 expandIcon.addEventListener('click', () => {
     if (expandedContent.style.display === 'none') {
         expandedContent.style.display = 'block';
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,3 +1,18 @@
+let ongo = sessionStorage.getItem('ongo');
+
+if(ongo == 'true'){
+   document.getElementById("changable").innerHTML="My Profile"
+   document.getElementById("changable").setAttribute("href", "myprofile");
+   document.getElementById("upload").style.display="flex";
+    document.getElementById("dropdown").style.display="flex";
+}
+else
+{
+    document.getElementById("upload").style.display="none";
+    document.getElementById("dropdown").style.display="none";
+}
+
+
 document.addEventListener("DOMContentLoaded", function () {
     let fileInput = document.getElementById("file-input");
     let fileList = document.getElementById("files-list");
