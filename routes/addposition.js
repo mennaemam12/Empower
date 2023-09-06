fd4e61e5392f6452221eeeda525512fd5592ee50
Mario Magdy <mariomagdyfa@gmail.com>
2023-09-06 15:50:24 +0300

    added

--- routes/addposition.js
+++ routes/addposition.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
+const multer = require('multer');
+const storage = multer.memoryStorage(); // Store files in memory
+const upload = multer({ storage: storage });
+const fileUpload = require('express-fileupload');
+const mongoose=require('mongoose');
+const path = require('path');
+router.use(fileUpload());
 const addposition=require("../controllers/addposition");
-router.use(bodyParser.json());
+
 
 router.get('/',(req,res)=>
 {
