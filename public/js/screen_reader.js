4bc32757e186f7e1d60f5568db7d142d76d08f2f
Mario Magdy <mariomagdyfa@gmail.com>
2023-09-06 20:26:03 +0300

    added

--- public/js/screen_reader.js
+++ public/js/screen_reader.js
@@ -49,17 +49,27 @@ function buttonPressed12() {
     const elements = document.querySelectorAll('.reading12');
 
     if (num % 2 === 0) {
+        let screenreader = document.getElementById("screenreader");
+        let screenlogo = document.getElementById("screenlogo");
+        screenlogo.style.transition = "color 0.5s ease";
+        screenreader.style.border = "1px solid green";
+        screenlogo.style.color="green";
         elements.forEach(element => {
             element.addEventListener('mouseenter', () => {
                 mouseenterHandler(element);
             });
-
             element.addEventListener('mouseleave', mouseleaveHandler);
-        });
+           
+            });
     } else {
         elements.forEach(element => {
             element.removeEventListener('mouseenter', mouseenterHandler);
             element.removeEventListener('mouseleave', mouseleaveHandler);
+            let screenreader = document.getElementById("screenreader");
+            screenlogo.style.transition = "color 0.5s ease";
+            screenreader.style.border = "0px solid green"; 
+            screenlogo.style.color="#25316D";
+            
         });
     }
 }
