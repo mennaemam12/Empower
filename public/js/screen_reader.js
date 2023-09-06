1e10993e7ae5f6f69959995e82adbde79601c89a
Mario Magdy <mariomagdyfa@gmail.com>
2023-09-06 19:51:11 +0300

    added

--- public/js/screen_reader.js
+++ public/js/screen_reader.js
@@ -1,45 +1,65 @@
-let hasBeenRead = false; 
-let speaking = false; 
+let num = 1;
+let hasBeenRead = false;
+let speaking = false;
 
 function speakText(text) {
     if ('speechSynthesis' in window) {
         const synth = window.speechSynthesis;
         const utterance = new SpeechSynthesisUtterance(text);
 
-   
         if (speaking) {
             synth.cancel();
         }
 
         synth.speak(utterance);
-        speaking = true; 
+        speaking = true;
 
-        utterance.onend = function() {
-            speaking = false; 
+        utterance.onend = function () {
+            speaking = false;
         };
     } else {
         alert('Text-to-speech is not supported in this browser.');
     }
 }
 
-document.querySelectorAll('.reading12').forEach(element => {
-    element.addEventListener('mouseenter', () => {
-        if (!hasBeenRead) {
-            const text = element.textContent;
-            speakText(text);
-            hasBeenRead = true; 
-        }
-    });
+function mouseenterHandler(element) {
+    if (!hasBeenRead) {
+        const text = element.textContent;
+        speakText(text);
+        hasBeenRead = true;
+    }
+}
 
-    element.addEventListener('mouseleave', () => {
-        if (speaking) {
-            const synth = window.speechSynthesis;
-            synth.cancel();
-            speaking = false;
-        }
+function mouseleaveHandler() {
+    if (speaking) {
+        const synth = window.speechSynthesis;
+        synth.cancel();
+        speaking = false;
+    }
 
-        setTimeout(() => {
-            hasBeenRead = false;
-        }, 10);
-    });
-});
+    setTimeout(() => {
+        hasBeenRead = false;
+    }, 10);
+}
+
+function buttonPressed12() {
+    num++;
+    console.log(num);
+
+    const elements = document.querySelectorAll('.reading12');
+
+    if (num % 2 === 0) {
+        elements.forEach(element => {
+            element.addEventListener('mouseenter', () => {
+                mouseenterHandler(element);
+            });
+
+            element.addEventListener('mouseleave', mouseleaveHandler);
+        });
+    } else {
+        elements.forEach(element => {
+            element.removeEventListener('mouseenter', mouseenterHandler);
+            element.removeEventListener('mouseleave', mouseleaveHandler);
+        });
+    }
+}
