3ac556893accdfe388c4d1b096a8e0dbe856bac4
Youssef2022003 <youssef.sherif03@gmail.com>
2023-09-07 06:05:04 +0300

    about

--- routes/aboutUs.js
+++ routes/aboutUs.js
@@ -29,7 +29,7 @@ router.get('/', async(req,res)=>
       }
     
     };
-    res.render('about',notificationMessages);
+    res.render('about',{notificationMessages});
 })
 
 module.exports = router;
